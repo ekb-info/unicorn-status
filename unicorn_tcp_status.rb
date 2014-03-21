4fb0f2ef5bf9fb02a9fae9f15ef9a396634a6f10
Andrey Volkov <volkov.am@ekb-info.ru>
2014-03-21 17:40:03 +0600

    Integrate "HT queue"

--- unicorn_tcp_status.rb
+++ unicorn_tcp_status.rb
@@ -5,9 +5,9 @@ require 'unicorn'
 
 # Usage for this program
 def usage
-  puts "ruby unicorn_status.rb <path to unix socket> <poll interval in seconds>"
-  puts "Polls the given Unix socket every interval in seconds. Will not allow you to drop below 3 second poll intervals."
-  puts "Example: ruby unicorn_status.rb /var/run/engineyard/unicorn_appname.sock 10"
+  puts "ruby unicorn_status.rb <path to unix socket> <poll interval in seconds> [number of iterations]"
+  puts "Polls the given TCP socket every interval in seconds. Will not allow you to drop below 1 second poll intervals."
+  puts "Example: ruby unicorn_tcp_status.rb 0.0.0.0:8887 10 6"
 end
 
 # Look for required args. Throw usage and exit if they don't exist.
@@ -25,12 +25,18 @@ if threshold.to_i < 1
   threshold = 1
 end
 
+if ARGV.count >= 2
+  count = (ARGV[2]).to_i
+else
+  count = -1
+end
+
 # Poll the given socket every THRESHOLD seconds as specified above.
 puts "Running infinite loop. Use CTRL+C to exit."
 puts "------------------------------------------"
 loop do
   Raindrops::Linux.tcp_listener_stats(socket).each do |addr, stats|
-    puts "Run procs: " + `vmstat |grep -o '^ \\?[0-9]\\+'`.delete!("\n").to_i.to_s + "	Active reqs: " + stats.active.to_s + "	Queued reqs: " + stats.queued.to_s 
+    puts "Run procs: " + `vmstat |grep -o '^ \\?[0-9]\\+'`.delete!("\n").to_i.to_s + "	HT queue: " + `./siblings.sh`.delete!("\n") + "	Active reqs: " + stats.active.to_s + "	Queued reqs: " + stats.queued.to_s 
   end
   sleep threshold
 end
